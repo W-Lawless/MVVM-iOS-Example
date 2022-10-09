06740a8c31604e63cc6bf760022e107a87662f14
W Lawless <wLawless.sharpe@protonmail.com>
2022-10-08 21:13:43 -0400

    init new project

---
+++ MVVM-iOS-Example/MVVM-iOS-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-iOS-Example
+//
+//  Created by W Lawless on 10/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
